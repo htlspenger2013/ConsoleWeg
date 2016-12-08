5f5da187f9989f7200775cdd9bae03c35072f417
Tschernko <tschernko@spengergasse.at>
2016-12-08 18:39:58 +0100

    Add project files.

---
+++ src/Wegwerf.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wegwerf.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IList<int> list = new List<int>();
+            ISet<int> set = new HashSet<int>();
+            Console.WriteLine("hallo");
+
+            Random r = new Random();
+            while(set.Count < 20) {
+                set.Add(r.Next(1, 21));
+            }
+            foreach (var item in set) {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
